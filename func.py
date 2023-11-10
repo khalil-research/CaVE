74da69d4232a9964ea2c299499f62ffef4f5b339
LucasBoTang <botang@mie.utoronto.ca>
2023-11-10 10:36:41 -0500

    New method: average

--- func.py
+++ func.py
@@ -100,7 +100,7 @@ class exactConeAlignedCosine(nn.Module):
 
 class baseVectConeAlignedCosine(nn.Module):
     """
-    A autograd module to align cone and vector cosine similarity loss via vase vectors
+    A autograd module to align cone and vector cosine similarity loss via base vectors
     """
 
     def __init__(self, optmodel):
@@ -149,6 +149,61 @@ class baseVectConeAlignedCosine(nn.Module):
         # get max cosine similarity for each sample
         max_cos_sim, _ = torch.max(cos_sim, dim=1)
         loss = - max_cos_sim
+        return loss
+
+
+class avgVectConeAlignedCosine(nn.Module):
+    """
+    A autograd module to align cone and vector cosine similarity loss via average base vectors
+    """
+
+    def __init__(self, optmodel):
+        """
+        Args:
+            optmodel (optModel): an PyEPO optimization model
+        """
+        super().__init__()
+        # optimization model
+        if not isinstance(optmodel, optModel):
+            raise TypeError("arg model is not an optModel")
+        self.optmodel = optmodel
+
+    def forward(self, pred_cost, tight_ctrs, reduction="mean"):
+        """
+        Forward pass
+        """
+        loss = self._calLoss(pred_cost, tight_ctrs, self.optmodel)
+        # reduction
+        if reduction == "mean":
+            loss = torch.mean(loss)
+        elif reduction == "sum":
+            loss = torch.sum(loss)
+        elif reduction == "none":
+            loss = loss
+        else:
+            raise ValueError("No reduction '{}'.".format(reduction))
+        return loss
+
+    def _calLoss(self, pred_cost, tight_ctrs, optmodel):
+        """
+        A method to calculate loss
+        """
+        # get device
+        device = pred_cost.device
+        # get batch size
+        batch_size = len(pred_cost)
+        # init loss
+        loss = torch.empty(batch_size).to(device)
+        # cost vectors direction
+        if optmodel.modelSense == EPO.MINIMIZE:
+            # minimize
+            pred_cost = - pred_cost
+        # calculate cosine similarity
+        cos_sim = F.cosine_similarity(pred_cost.unsqueeze(1),
+                                      tight_ctrs.mean(axis=0).unsqueeze(0),
+                                      dim=2)
+        # get max cosine similarity for each sample
+        loss = - cos_sim
         return loss
 
 
