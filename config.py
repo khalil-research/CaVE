44444193dd286819d1ce29b8dfcacde147cff3f8
Bo Tang <botang@mie.utoronto.ca>
2023-12-07 14:27:32 -0500

    Add timeout_min

--- config.py
+++ config.py
@@ -16,18 +16,21 @@ configs["sp5"] = {}
 configs["sp5"]["2s"] = SimpleNamespace()
 configs["sp5"]["2s"].lr = 1e-2
 configs["sp5"]["2s"].epochs = 20
+configs["sp5"]["2s"].timeout_min = 2
 
 # CaVE
 configs["sp5"]["cave"] = SimpleNamespace()
 configs["sp5"]["cave"].lr = 1e-2
 configs["sp5"]["cave"].epochs = 10
 configs["sp5"]["cave"].solver = "nnls"
+configs["sp5"]["cave"].timeout_min = 3
 
 # CaVE+
 configs["sp5"]["cave+"] = SimpleNamespace()
 configs["sp5"]["cave+"].lr = 1e-2
 configs["sp5"]["cave+"].epochs = 10
 configs["sp5"]["cave+"].solver = "nnls"
+configs["sp5"]["cave+"].timeout_min = 3
 
 # CaVE Hybrid
 configs["sp5"]["caveh"] = SimpleNamespace()
@@ -36,11 +39,13 @@ configs["sp5"]["caveh"].epochs = 10
 configs["sp5"]["caveh"].solver = "nnls"
 configs["sp5"]["caveh"].solve_ratio = 0.3
 configs["sp5"]["caveh"].inner_ratio = 0.2
+configs["sp5"]["caveh"].timeout_min = 2
 
 # SPO+
 configs["sp5"]["spo+"] = SimpleNamespace()
 configs["sp5"]["spo+"].lr = 1e-2
 configs["sp5"]["spo+"].epochs = 10
+configs["sp5"]["spo+"].timeout_min = 3
 
 # PFYL
 configs["sp5"]["pfyl"] = SimpleNamespace()
@@ -48,13 +53,14 @@ configs["sp5"]["pfyl"].lr = 1e-2
 configs["sp5"]["pfyl"].epochs = 10
 configs["sp5"]["pfyl"].n_samples = 1
 configs["sp5"]["pfyl"].sigma = 1.0
+configs["sp5"]["pfyl"].timeout_min = 3
 
 # NCE
 configs["sp5"]["nce"] = SimpleNamespace()
 configs["sp5"]["nce"].lr = 1e-2
-configs["sp5"]["nce"].epochs = 10
+configs["sp5"]["nce"].epochs = 20
 configs["sp5"]["nce"].solve_ratio = 0.05
-
+configs["sp5"]["nce"].timeout_min = 3
 
 ### ========================= TSP20 =========================
 configs["tsp20"] = {}
@@ -63,18 +69,21 @@ configs["tsp20"] = {}
 configs["tsp20"]["2s"] = SimpleNamespace()
 configs["tsp20"]["2s"].lr = 5e-2
 configs["tsp20"]["2s"].epochs = 20
+configs["tsp20"]["2s"].timeout_min = 3
 
 # CaVE
 configs["tsp20"]["cave"] = SimpleNamespace()
 configs["tsp20"]["cave"].lr = 5e-2
 configs["tsp20"]["cave"].epochs = 10
 configs["tsp20"]["cave"].solver = "clarabel"
+configs["tsp20"]["cave"].timeout_min = 5
 
 # CaVE+
 configs["tsp20"]["cave+"] = SimpleNamespace()
 configs["tsp20"]["cave+"].lr = 5e-2
 configs["tsp20"]["cave+"].epochs = 10
 configs["tsp20"]["cave+"].solver = "clarabel"
+configs["tsp20"]["cave+"].timeout_min = 5
 
 # CaVE Hybrid
 configs["tsp20"]["caveh"] = SimpleNamespace()
@@ -83,11 +92,13 @@ configs["tsp20"]["caveh"].epochs = 10
 configs["tsp20"]["caveh"].solver = "clarabel"
 configs["tsp20"]["caveh"].solve_ratio = 0.3
 configs["tsp20"]["caveh"].inner_ratio = 0.2
+configs["tsp20"]["caveh"].timeout_min = 3
 
 # SPO+
 configs["tsp20"]["spo+"] = SimpleNamespace()
 configs["tsp20"]["spo+"].lr = 5e-2
 configs["tsp20"]["spo+"].epochs = 10
+configs["tsp20"]["spo+"].timeout_min = 5
 
 # PFYL
 configs["tsp20"]["pfyl"] = SimpleNamespace()
@@ -95,12 +106,14 @@ configs["tsp20"]["pfyl"].lr = 5e-2
 configs["tsp20"]["pfyl"].epochs = 10
 configs["tsp20"]["pfyl"].n_samples = 1
 configs["tsp20"]["pfyl"].sigma = 1.0
+configs["tsp20"]["pfyl"].timeout_min = 5
 
 # NCE
 configs["tsp20"]["nce"] = SimpleNamespace()
 configs["tsp20"]["nce"].lr = 5e-2
-configs["tsp20"]["nce"].epochs = 10
+configs["tsp20"]["nce"].epochs = 20
 configs["tsp20"]["nce"].solve_ratio = 0.05
+configs["tsp20"]["nce"].timeout_min = 5
 
 
 ### ========================= TSP50 =========================
@@ -110,18 +123,21 @@ configs["tsp50"] = {}
 configs["tsp50"]["2s"] = SimpleNamespace()
 configs["tsp50"]["2s"].lr = 5e-2
 configs["tsp50"]["2s"].epochs = 20
+configs["tsp50"]["2s"].timeout_min = 30
 
 # CaVE
 configs["tsp50"]["cave"] = SimpleNamespace()
 configs["tsp50"]["cave"].lr = 5e-2
 configs["tsp50"]["cave"].epochs = 10
 configs["tsp50"]["cave"].solver = "clarabel"
+configs["tsp50"]["cave"].timeout_min = 30
 
 # CaVE+
 configs["tsp50"]["cave+"] = SimpleNamespace()
 configs["tsp50"]["cave+"].lr = 5e-2
 configs["tsp50"]["cave+"].epochs = 10
 configs["tsp50"]["cave+"].solver = "clarabel"
+configs["tsp50"]["cave+"].timeout_min = 30
 
 # CaVE Hybrid
 configs["tsp50"]["caveh"] = SimpleNamespace()
@@ -130,11 +146,13 @@ configs["tsp50"]["caveh"].epochs = 10
 configs["tsp50"]["caveh"].solver = "clarabel"
 configs["tsp50"]["caveh"].solve_ratio = 0.3
 configs["tsp50"]["caveh"].inner_ratio = 0.2
+configs["tsp50"]["caveh"].timeout_min = 20
 
 # SPO+
 configs["tsp50"]["spo+"] = SimpleNamespace()
 configs["tsp50"]["spo+"].lr = 5e-2
 configs["tsp50"]["spo+"].epochs = 10
+configs["tsp50"]["spo+"].timeout_min = 30
 
 # PFYL
 configs["tsp50"]["pfyl"] = SimpleNamespace()
@@ -142,9 +160,11 @@ configs["tsp50"]["pfyl"].lr = 5e-2
 configs["tsp50"]["pfyl"].epochs = 10
 configs["tsp50"]["pfyl"].n_samples = 1
 configs["tsp50"]["pfyl"].sigma = 1.0
+configs["tsp50"]["pfyl"].timeout_min = 30
 
 # NCE
 configs["tsp50"]["nce"] = SimpleNamespace()
 configs["tsp50"]["nce"].lr = 5e-2
-configs["tsp50"]["nce"].epochs = 10
+configs["tsp50"]["nce"].epochs = 20
 configs["tsp50"]["nce"].solve_ratio = 0.05
+configs["tsp50"]["nce"].timeout_min = 30
