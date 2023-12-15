af3ebf39cb7912843a8f2427fe6c129af86101eb
LucasBoTang <botang@mie.utoronto.ca>
2023-12-15 12:15:18 -0500

    New test: multi-process

--- test/test_func.py
+++ test/test_func.py
@@ -34,26 +34,40 @@ class TestExactConeAlignedCosine(unittest.TestCase):
         A test to check the loss forward pass goes well with scipy.nnls.
         """
         # init loss
-        loss = exactConeAlignedCosine(self.mock_optmodel, solver="nnls")
+        cave = exactConeAlignedCosine(self.mock_optmodel, solver="nnls")
         # forward pass
-        loss(self.mock_costs, self.mock_bctrs)
+        loss = cave(self.mock_costs, self.mock_bctrs)
         # cuda exist or not
         if torch.cuda.is_available():
             # forward pass with cuda
-            loss(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+            cave(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+        # multiprocessing
+        cave = exactConeAlignedCosine(self.mock_optmodel, solver="nnls", processes=4)
+        # forward pass
+        loss_mp = cave(self.mock_costs, self.mock_bctrs)
+        # check result consistency
+        self.assertTrue(torch.isclose(loss, loss_mp).item(),
+                        "Inconsistent results with multiple processes.")
 
     def testClarabel(self):
         """
         A test to check the loss forward pass goes well with Clarabel.
         """
         # init loss
-        loss = exactConeAlignedCosine(self.mock_optmodel, solver="clarabel")
+        cave = exactConeAlignedCosine(self.mock_optmodel, solver="clarabel")
         # forward pass
-        loss(self.mock_costs, self.mock_bctrs)
+        loss = cave(self.mock_costs, self.mock_bctrs)
         # cuda exist or not
         if torch.cuda.is_available():
             # forward pass with cuda
-            loss(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+            cave(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+        # multiprocessing
+        cave = exactConeAlignedCosine(self.mock_optmodel, solver="clarabel", processes=4)
+        # forward pass
+        loss_mp = cave(self.mock_costs, self.mock_bctrs)
+        # check result consistency
+        self.assertTrue(torch.isclose(loss, loss_mp).item(),
+                        "Inconsistent results with multiple processes.")
 
 
 class TestInnerConeAlignedCosine(unittest.TestCase):
@@ -74,52 +88,81 @@ class TestInnerConeAlignedCosine(unittest.TestCase):
         A test to check the loss forward pass goes well with scipy.nnls.
         """
         # init loss
-        loss = innerConeAlignedCosine(self.mock_optmodel, solver="nnls")
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="nnls")
         # forward pass
-        loss(self.mock_costs, self.mock_bctrs)
+        loss = cave(self.mock_costs, self.mock_bctrs)
         # cuda exist or not
         if torch.cuda.is_available():
             # forward pass with cuda
-            loss(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+            cave(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+        # multiprocessing
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="nnls", processes=4)
+        # forward pass
+        loss_mp = cave(self.mock_costs, self.mock_bctrs)
+        # check result consistency
+        self.assertTrue(torch.isclose(loss, loss_mp).item(),
+                        "Inconsistent results with multiple processes.")
+
 
     def testNNLSPartial(self):
         """
         A test to check the loss forward pass goes well with heuristic proejection.
         """
         # init loss
-        loss = innerConeAlignedCosine(self.mock_optmodel, solver="nnls", solve_ratio=0)
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="nnls", solve_ratio=0)
         # forward pass
-        loss(self.mock_costs, self.mock_bctrs)
+        loss = cave(self.mock_costs, self.mock_bctrs)
         # cuda exist or not
         if torch.cuda.is_available():
             # forward pass with cuda
-            loss(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+            cave(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+        # multiprocessing
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="nnls", solve_ratio=0, processes=4)
+        # forward pass
+        loss_mp = cave(self.mock_costs, self.mock_bctrs)
+        # check result consistency
+        self.assertTrue(torch.isclose(loss, loss_mp).item(),
+                        "Inconsistent results with multiple processes.")
 
     def testClarabel(self):
         """
         A test to check the loss forward pass goes well with Clarabel.
         """
         # init loss
-        loss = innerConeAlignedCosine(self.mock_optmodel, solver="clarabel")
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="clarabel")
         # forward pass
-        loss(self.mock_costs, self.mock_bctrs)
+        loss = cave(self.mock_costs, self.mock_bctrs)
         # cuda exist or not
         if torch.cuda.is_available():
             # forward pass with cuda
-            loss(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+            cave(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+        # multiprocessing
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="clarabel", processes=4)
+        # forward pass
+        loss_mp = cave(self.mock_costs, self.mock_bctrs)
+        # check result consistency
+        self.assertTrue(torch.isclose(loss, loss_mp).item(),
+                        "Inconsistent results with multiple processes.")
 
     def testClarabelPartial(self):
         """
         A test to check the loss forward pass goes well with heuristic proejection.
         """
         # init loss
-        loss = innerConeAlignedCosine(self.mock_optmodel, solver="clarabel", solve_ratio=0)
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="clarabel", solve_ratio=0)
         # forward pass
-        loss(self.mock_costs, self.mock_bctrs)
+        loss = cave(self.mock_costs, self.mock_bctrs)
         # cuda exist or not
         if torch.cuda.is_available():
             # forward pass with cuda
-            loss(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+            cave(self.mock_costs.cuda(), self.mock_bctrs.cuda())
+        # multiprocessing
+        cave = innerConeAlignedCosine(self.mock_optmodel, solver="clarabel", processes=4, solve_ratio=0)
+        # forward pass
+        loss_mp = cave(self.mock_costs, self.mock_bctrs)
+        # check result consistency
+        self.assertTrue(torch.isclose(loss, loss_mp).item(),
+                        "Inconsistent results with multiple processes.")
 
 
 if __name__ == "__main__":
