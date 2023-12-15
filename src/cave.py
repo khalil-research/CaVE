be3c49e91642a332c8baf3a7eb6a27596762c31b
LucasBoTang <botang@mie.utoronto.ca>
2023-12-15 11:15:07 -0500

    Docs

--- src/cave.py
+++ src/cave.py
@@ -19,7 +19,7 @@ from pyepo.model.opt import optModel
 
 class abstractConeAlignedCosine(nn.Module, ABC):
     """
-    Abstract base class for CaVE loss.
+    An abstract base class for CaVE loss.
     """
     def __init__(self, optmodel, reduction="mean", processes=1):
         """
@@ -34,18 +34,20 @@ class abstractConeAlignedCosine(nn.Module, ABC):
             raise TypeError("arg model is not an optModel")
         # cost vectors direction
         if optmodel.modelSense == EPO.MINIMIZE:
-            # minimize
+            # set vector sign to -1 for minimization
             self.vec_sign = -1
         else:
-            # maximize
+            # set vector sign to 1 for maximization
             self.vec_sign = 1
-        # how to aggregate loss
+        # method for aggregating the loss
         self.reduction = reduction
+        if self.reduction not in ["mean", "sum", "none"]:
+            message = ValueError("No reduction '{}'.".format(self.reduction))
         # number of processes
         self.processes = mp.cpu_count() if not processes else processes
         # multi-core
         if self.processes > 1:
-            # create processes pool
+            # create a processes pool
             self.pool = ProcessingPool(self.processes)
         print("Num of cores: {}".format(self.processes))
 
@@ -54,15 +56,13 @@ class abstractConeAlignedCosine(nn.Module, ABC):
         A Forward pass method.
         """
         loss = self._calLoss(pred_cost, tight_ctrs)
-        # reduction
+        # loss reduction
         if self.reduction == "mean":
             loss = torch.mean(loss)
-        elif self.reduction == "sum":
+        if self.reduction == "sum":
             loss = torch.sum(loss)
-        elif self.reduction == "none":
+        if self.reduction == "none":
             loss = loss
-        else:
-            raise ValueError("No reduction '{}'.".format(reduction))
         return loss
 
     def _calLoss(self, pred_cost, tight_ctrs):
@@ -73,7 +73,7 @@ class abstractConeAlignedCosine(nn.Module, ABC):
         pred_cost = self.vec_sign * pred_cost
         # get projection
         proj = self._getProjection(pred_cost, tight_ctrs)
-        # calculate cosine similarity
+        # calculate cosine similarity between predicted costs and their projections
         loss = - F.cosine_similarity(pred_cost, proj, dim=1)
         return loss
 
@@ -87,7 +87,7 @@ class abstractConeAlignedCosine(nn.Module, ABC):
 
 class exactConeAlignedCosine(abstractConeAlignedCosine):
     """
-    A autograd module for CaVE Exact
+    An autograd module for CaVE Exact
     """
     def __init__(self, optmodel, solver="clarabel", reduction="mean", processes=1):
         """
@@ -98,13 +98,15 @@ class exactConeAlignedCosine(abstractConeAlignedCosine):
             processes (int): number of processors, 1 for single-core, 0 for all of coress
         """
         super().__init__(optmodel, reduction, processes)
-        # solver
+        # choose between 'clarabel' or 'nnls' as the QP solver.
         self.solver = solver
         if self.solver not in ["clarabel", "nnls"]:
             message = "Invalid solver: {}. Must be 'clarabel' or 'nnls'.".format(self.solver)
             raise ValueError(message)
+        # clarabel which has better scalability
         if self.solver == "clarabel":
             self._solveQP = self._solveClarabel
+        # nnls from scipy which is very fast for small problems
         if self.solver == "nnls":
             self._solveQP = self._solveNNLS
 
@@ -112,7 +114,7 @@ class exactConeAlignedCosine(abstractConeAlignedCosine):
         """
         A method to get the exact projection onto the optimal subcone.
         """
-        # get device
+        # get device for output tensor placement
         device = pred_cost.device
         # to numpy
         pred_cost = pred_cost.detach().cpu().numpy()
@@ -149,40 +151,40 @@ class exactConeAlignedCosine(abstractConeAlignedCosine):
         """
         A static method to solve quadratic programming with Clarabel
         """
-        # drop 0-pads for binding constraints
+        # remove zero-padding from binding constraints
         ctr = ctr[np.abs(ctr).sum(axis=1) > 1e-7]
         # decision varibles λ
         λ = cvx.Variable(len(ctr), name="λ", nonneg=True)
         # onjective function ||cp - λ ctr||^2
         objective = cvx.Minimize(cvx.sum_squares(cp - λ @ ctr))
-        # ceate a model
+        # ceate an optimization problem model
         problem = cvx.Problem(objective)
         # solve with Clarabel
         problem.solve(solver=cvx.CLARABEL)
-        # get closest projection (which is on the surface of the cone)
+        # obtain the closest projection on the surface of the cone
         p = λ.value @ ctr
-        # get residuals (Euclidian distance between prediction and projection)
+        # compute residuals as the Euclidean distance between prediction and projection
         rnorm = problem.value
-        return torch.FloatTensor(p), rnorm
+        return torch.tensor(p), rnorm
 
     @staticmethod
     def _solveNNLS(cp, ctr):
         """
         A static method to solve quadratic programming with scipy
         """
-        # drop 0-pads for binding constraints
+        # remove zero-padding from binding constraints
         ctr = ctr[np.abs(ctr).sum(axis=1) > 1e-7]
-        # solve as non-negaitive least square (active set method)
+        # solve as non-negaitive least square (using the active set method)
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.nnls.html
         λ, rnorm = nnls(ctr.T, cp)
-        # get closest projection (which is on the surface of the cone)
+        # compute residuals as the Euclidean distance between prediction and projection
         p = λ @ ctr
-        return torch.FloatTensor(p), rnorm
+        return torch.tensor(p), rnorm
 
 
 class innerConeAlignedCosine(exactConeAlignedCosine):
     """
-    A autograd module for CaVE+ and CaVE Hybrid.
+    An autograd module for CaVE+ and CaVE Hybrid.
     """
     def __init__(self, optmodel, solver="clarabel", max_iter=3, solve_ratio=1,
                  inner_ratio=0.2, reduction="mean", processes=1):
@@ -201,13 +203,13 @@ class innerConeAlignedCosine(exactConeAlignedCosine):
         self.max_iter = max_iter
         # solve ratio
         self.solve_ratio = solve_ratio
-        # check if value is valid (0 - 1)
+        # check if value is valid [0,1]
         if (self.solve_ratio < 0) or (self.solve_ratio > 1):
             raise ValueError("Invalid solving ratio {}. It should be between 0 and 1.".
                 format(self.solve_ratio))
         # inner ratio
         self.inner_ratio = inner_ratio
-        # check if value is valid (0 - 1)
+        # check if value is valid [0,1]
         if (self.inner_ratio < 0) or (self.inner_ratio > 1):
             raise ValueError("Invalid inner ratio {}. It should be between 0 and 1.".
                 format(self.inner_ratio))
@@ -246,9 +248,11 @@ class innerConeAlignedCosine(exactConeAlignedCosine):
                 rnorm = torch.tensor(rnorm).to(device)
             # normalize
             proj = proj / proj.norm(dim=1, keepdim=True)
+            # limit the max_iter for clarabel
             if self.solver == "clarabel":
                 # already get inner projection
                 vec = proj
+            # manually push projection inside for scipy.nnls
             else:
                 # get excact projection
                 # push vector inside by a convex combination of projection and average
@@ -278,36 +282,36 @@ class innerConeAlignedCosine(exactConeAlignedCosine):
         """
         A static method to solve quadratic programming with Clarabel
         """
-        # drop 0-pads for binding constraints
+        # remove zero-padding from binding constraints
         ctr = ctr[np.abs(ctr).sum(axis=1) > 1e-7]
         # decision varibles λ
         λ = cvx.Variable(len(ctr), name="λ", nonneg=True)
         # onjective function ||cp - λ ctr||^2
         objective = cvx.Minimize(cvx.sum_squares(cp - λ @ ctr))
-        # ceate a model
+        # ceate an optimization problem model
         problem = cvx.Problem(objective)
         # solve with Clarabel with limited iteration for the suboptimal
         problem.solve(solver=cvx.CLARABEL, max_iter=max_iter)
         # get inner projection (which is inside the cone)
         p = λ.value @ ctr
-        # get residuals (Euclidian distance between prediction and projection)
+        # compute residuals as the Euclidean distance between prediction and projection
         rnorm = problem.value
-        return torch.FloatTensor(p), rnorm
+        return torch.tensor(p), rnorm
 
     @staticmethod
     def _solveNNLS(cp, ctr, max_iter):
         """
         A static method to solve quadratic programming with scipy
         """
-        # WARNING: set max_iter will lead infeasibe and the solution will not be available
-        # Now, the max_iter is set to unlimited, but the project will be push inside
+        # WARNING: set max_iter will lead infeasibility and the solution will not be available
+        # Now, the max_iter is set to unlimited, but the project will be push inside later
         # by a convex combination of exact projection and average binding constraints
         max_iter = None
         # drop pads
         ctr = ctr[np.abs(ctr).sum(axis=1) > 1e-7]
-        # solve as non-negaitive least square (active set method)
+        # solve as non-negaitive least square (using the active set method)
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.nnls.html
         λ, rnorm = nnls(ctr.T, cp, maxiter=max_iter)
-        # get closest projection (which is on the surface of the cone)
+        # obtain the closest projection on the surface of the cone
         p = λ @ ctr
-        return torch.FloatTensor(p), rnorm
+        return torch.tensor(p), rnorm
